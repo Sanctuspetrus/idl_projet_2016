eb355e9fa9f83628854e349dd0adc025e4f06a70
Donasian Le Bronnec <lebronnecdonasian@gmail.com>
2016-03-18 09:22:40 +0100

    Test Unitaire package Utilitaire v2

--- WorkspaceJava/tutorial/src/main/java/testUtilitaire/ConversionImpl.java
+++ WorkspaceJava/tutorial/src/main/java/testUtilitaire/ConversionImpl.java
@@ -2,13 +2,13 @@ package testUtilitaire;
 
 public class ConversionImpl {
 	
-	public static double getForceFromUV(double u, double v){
-		return Math.sqrt(Math.pow(u, 2)+Math.pow(v, 2));
+	public static float getForceFromUV(float u, float v){
+		return (float) Math.sqrt(Math.pow(u, 2)+Math.pow(v, 2));
 		
 	}
 	
-	public static double getAngleFromUV(double u, double v){
-		return Math.toDegrees(Math.atan2(v, u));
+	public static float getAngleFromUV(float u, float v){
+		return (float) Math.toDegrees(Math.atan2(v, u));
 		
 	}
 	
---
+++ WorkspaceJava/tutorial/src/main/java/testUtilitaire/Vent.java
@@ -0,0 +1,16 @@
+package testUtilitaire;
+
+public class Vent {
+
+	float u;
+	float v;
+	float pression;
+	
+	public Vent(float u, float v, float pression) {
+		super();
+		this.u = u;
+		this.v = v;
+		this.pression = pression;
+	}
+	
+}
